7a9b6e4e01acc1e5e6b0108196b1243454e21f84
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-25 15:17:58 +0800

    Split the name and gwa using split function

--- Determine the highest GWA.py
+++ Determine the highest GWA.py
@@ -2,3 +2,7 @@
 #open and read the files line by line
 with open("Name and gwa.txt") as achiever:
     name_gwa = achiever.readlines()
+
+#Using the split function, separate the name and gwa
+for line in name_gwa:
+    name,gwa = line.split(",")
