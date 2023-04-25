d966d236d0f3f076b8952c10da722f534032d145
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-25 16:16:40 +0800

    Code fixed and output the desired output

--- Determine the highest GWA.py
+++ Determine the highest GWA.py
@@ -3,25 +3,26 @@
 with open("Name and gwa.txt") as achiever:
     name_gwa = achiever.readlines()
 
+#Input the highest grade a student can get
+highest_grade = 5
+
 #Using the split function, separate the name and gwa
 for line in name_gwa:
     name,gwa = line.split(",")
 
     #Convert the gwa into a float
     gwa = float(gwa)
-#Input the highest grade a student can get
-highest_grade = 5
 
-#compare the gwa with the max grade
-if gwa < highest_grade:
-    highest_grade = gwa
+    #compare the gwa with the max grade
+    if gwa < highest_grade:
+        highest_grade = gwa
 
     #to output the name of the student with highest grade
-    highest_student = name
+        highest_student = name
 
 #Print the highest grade
 print("")
 print("The highest GWA is:", highest_grade)
 print("")
-print("The name of the Highest student is:", name)
+print("The name of the Highest student is:", highest_student)
 print("")
